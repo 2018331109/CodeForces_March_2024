0576641559b820bd45fcdefb64158867a98bea0b
Ahmadul Hasan (p) <49359716+2018331109@users.noreply.github.com>
2024-03-18 15:10:36 +0600

    Add files via upload

---
+++ Round-931_D2/A.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define neg cout << -1 << endl;
+#define pb push_back
+#define ss second
+#define ff first
+const int INF=1e18;
+const int md=998244353;
+
+void solve()
+{
+   int n;
+   cin>>n;
+   int a[n];
+   for(int i=0;i<n;i++)
+   {
+       cin>>a[i];
+   }
+   sort(a, a+n);
+   int ans=a[n-1]-a[0]+a[n-1]-a[1]+a[n-2]-a[0]+a[n-2]-a[1];
+   cout<<ans<<endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int test = 1;
+    cin >> test;
+
+    for (int tc = 1; tc <= test; tc++)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
